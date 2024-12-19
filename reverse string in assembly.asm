9c11fb84d6d6d5610fa8f3011cb37771b8bbc463
Md. Sanjid Alam Araf <sanjidaraf2002@gmail.com>
2024-12-19 12:45:57 +0600

    completed code

---
+++ reverse string in assembly.asm
@@ -0,0 +1,47 @@
+org 100h
+
+.DATA
+string db 'FARA, DIJNAS', 0 
+newline db 0Dh, 0Ah, '$'
+
+.CODE
+ 
+MAIN PROC
+    
+    mov ax, @DATA
+    mov ds, ax
+
+    lea si, string 
+    mov cx, 0
+     
+len_loop:
+
+    cmp byte ptr [si], 0 
+    je rev_start 
+    inc si 
+    inc cx
+    jmp len_loop
+
+rev_start:
+
+    dec si 
+
+rev_loop:
+
+    mov dl, [si] 
+
+    mov ah, 02h 
+    int 21h
+
+    dec si 
+    loop rev_loop
+
+    mov dx, offset newline
+    mov ah, 09h
+    int 21h
+
+    mov ah, 4ch 
+    int 21h
+
+MAIN ENDP
+END MAIN
